8b547c38f26425d866f144bd28e06c61c5193277
MigeulEliotte <m.eliotte18@gmail.com>
2024-09-15 18:26:06 -0400

    -m

--- assignment2.py
+++ assignment2.py
@@ -2,20 +2,68 @@ import argparse
 import urllib.request
 import logging
 import datetime
+import csv
 
 def downloadData(url):
     """Downloads the data"""
-    pass
+    return urllib.request.urlopen(url).read().decode('utf-8')
 
 def processData(file_content):
-    pass
+    """This processes the CSV data and logs errors"""
+    data_dict = {}
+    logger = logging.getLogger('assignment2')
+    csv_reader = csv.reader(file_content.splitlines())
+    
 
+    for linenum, row in enumerate(csv_reader, start=1):
 
+        person_id, name, birthday = row
+        try:
+            birthday = datetime.datetime.strptime(birthday, '%d/%m/%Y').date()
+            data_dict[int(person_id)] = (name, birthday)
+        except ValueError:
+            logger.error(f"Error processing line #{linenum} for ID #{person_id}")
+    
+    return data_dict
+#this communicates with the user
 def displayPerson(id, personData):
-    pass
+    """Display someone's information by ID number"""
+    if id in personData:
+        name, birthday = personData[id]
+        print(f"Person #{id} is {name} with a birthday of {birthday}")
+    else:
+        print("ID Not Found")
+
+def setupLogger():
+    """Sets up logging configuration"""
+    logging.basicConfig(filename='errors.log', level=logging.ERROR)
 
 def main(url):
     print(f"Running main with URL = {url}...")
+    setupLogger()
+
+
+
+    # this downloads the data
+    try:
+        csv_data = downloadData(url)
+    except urllib.error.URLError as e:
+        print(f"Failed to retrieve data: {e}")
+        return
+
+    # Process data
+    person_data = processData(csv_data)
+    
+    # Ask for user input
+    while True:
+        try:
+            user_input = int(input("Enter an ID # for lookup (negative number to exit): "))
+            if user_input <= 0:
+                break
+            displayPerson(user_input, person_data)
+        except ValueError:
+            print("Please enter a Valid Number.")
+
 
 
 if __name__ == "__main__":
